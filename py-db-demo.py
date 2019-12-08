787dbae7d9d8d00667d3aff7824b90031dfb70f0
yangxiy <yangxiying.2010@foxmail.com>
2019-12-08 15:00:54 +0800

    py查询mssql的demo

--- py-db-demo.py
+++ py-db-demo.py
@@ -6,7 +6,7 @@ import pymssql
 server = "127.0.0.1"    # 连接服务器地址
 user = "sa"             # 连接帐号
 password = "Sa123!"     # 连接密码
-dbname = "ESApp"
+dbname = "hzmd-demo"    #默认连接的数据库名
 
 #连接sqlserver语法
 # db=pymssql.connect(server,user,password,dbname)
@@ -48,11 +48,17 @@ dbname = "ESApp"
 
 
 with pymssql.connect(server, user, password, dbname) as conn:
-    with conn.cursor(as_dict=True) as cursor:   # 数据存放到字典中
+    with conn.cursor(as_dict=True) as cursor:   # 数据存放到字典中 指定了as_dict为True，则返回结果变为字典类型，这样就能通过列名来访问结果
         cursor.execute('select b.[库存单位] as dw,b.[原煤库存库容量] as ykr ,b.[原煤库存量] as ykc' +
             ',b.[精煤库存库容量] as jkr ,b.[精煤库存量小计] as jkc ,b.[副产品库存库容量] as fkr,b.[副产品总库存量] as fkc'+
             ',b.[煤泥库容量] as nkr,b.[煤泥库存量] as nkc'+
             ' from [dbo].[霍州煤电集团公司库存情况日报表_明细] B ' + 
             ' where B.RCId = %s', 'RC19120700085')
-        for row in cursor:
-            print("库存单位=%d, 原煤库容=%s, 原煤库存=%s, 精煤库容=%s, 精煤库存=%s, 副产品库容=%s, 副产品库存=%s, 煤泥库容=%s, 煤泥库存=%s" % (row['dw'], row['ykr'], row['ykc'], row['jkr'], row['jkc'], row['fkr'], row['fkr'], row['nkr'], row['nkr']))
+        c2_list = cursor.fetchall()
+        # for row in cursor:
+        #     print("库存单位=%s, 原煤库容=%s, 原煤库存=%s, 精煤库容=%s, 精煤库存=%s, 副产品库容=%s, 副产品库存=%s, 煤泥库容=%s, 煤泥库存=%s" % (row['dw'], row['ykr'], row['ykc'], row['jkr'], row['jkc'], row['fkr'], row['fkr'], row['nkr'], row['nkr']))
+
+print("=====================================")
+for row in c2_list:
+    print("库存单位=%s, 原煤库容=%s, 原煤库存=%s, 精煤库容=%s, 精煤库存=%s, 副产品库容=%s, 副产品库存=%s, 煤泥库容=%s, 煤泥库存=%s" % 
+    (row['dw'], row['ykr'], row['ykc'], row['jkr'], row['jkc'], row['fkr'], row['fkr'], row['nkr'], row['nkr']))
